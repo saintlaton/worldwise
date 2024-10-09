699800a4633428e066b178b71298c134ea3cc985
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-10-10 02:45:58 +0700

    fix:deploy bug

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const jsonServer = require("json-server");
-const path = require("path");
+import jsonServer from "json-server";
+import { join } from "path"; // import path join
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Helper to resolve __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "data", "cities.json")); // Correct path
+const router = jsonServer.router(join(__dirname, "data", "cities.json")); // Correct path
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
